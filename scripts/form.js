8f4dbf545c0a8c2b7a8561b56db545a836a286a6
Nyhm13 <ryo131995@gmail.com>
2025-02-19 18:29:30 +0100

    adds tweaks from teacher

--- scripts/form.js
+++ scripts/form.js
@@ -2,7 +2,7 @@ const myForm = document.querySelector("form");
 
 myForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log("Prodotto aggiunto al carello");
+  console.log("Prodotto aggiunto al carrello");
 
   const prodottiInput = document.getElementById("prodotti");
   const quantitaInput = document.getElementById("quantita");
@@ -15,19 +15,27 @@ myForm.addEventListener("submit", function (e) {
 
   const prodotto = document.createElement("li");
   prodotto.classList.add("no-bullets");
-  prodotto.addEventListener("click", function () {
-    prodotto.classList.add("middle-line");
-  });
-  prodotto.innerText = `${formValues.prodotti} ${formValues.quantita}`;
+//   prodotto.addEventListener("click", function () {
+//     prodotto.classList.add("middle-line");
+//   });
+  prodotto.innerHTML = `<span onclick="barrami(event)">${formValues.prodotti} ${formValues.quantita}</span>`;
 
-  const nostroCarrello = document.getElementById("carello");
+  const nostroCarrello = document.getElementById("carrello");
 
   nostroCarrello.appendChild(prodotto);
   const deleteButton = document.createElement("button");
-  deleteButton.innerText = "Rimuovi dal carello";
+  deleteButton.innerText = "Rimuovi dal carrello";
   prodotto.appendChild(deleteButton);
-  
+
   deleteButton.addEventListener("click", function () {
     prodotto.remove();
   });
 });
+
+const barrami = function(e){
+  // e.target.classList.add("middle-line") seleziona l'elemento cliccato
+  // e.target.parentElement.remove() seleziona il parente dell'elemento cliccato
+    e.target.classList.add("middle-line"); 
+    
+  
+  }
